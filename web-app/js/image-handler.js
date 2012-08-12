faef753cfdeddaef9e589ae4414520fcd672c76d
markew <mark.woolston@axon.com.au>
2012-08-12 07:11:28 +0000

    refactor exif lookups encapsulate screen list of images
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@42 b53174df-531f-34c8-f11e-a8fbec9bff15

--- web-app/js/image-handler.js
+++ web-app/js/image-handler.js
@@ -1,17 +1,29 @@
 /* Handles image selection, uploading and extraction of embedded data. */
 
+var ExifLoader = {};
+
 $(function() {
 
     /* ExifLoader handles the extraction of EXIF data from any files that are added to the upload list. */
-    var ExifLoader = {
+    ExifLoader = {
         geocoder: new google.maps.Geocoder(),
-        onFileSelect: function (event) {
-            var that = event.data.that;
+        dataCache: {},
+
+        onRowAdded: function (files) {
+            //console.log("onRowAdded: name=" + files[0].name);
+
+            imageList.rowAdded();
+
             if (ExifLoader.geocoder === undefined) { ExifLoader.geocoder = new google.maps.Geocoder(); }
-            $(this).fileExif(function (exifObject) {
+
+            var dummy = [{files: files}];
+            //$('#files').getExif(files[0], function (exifObject) {
+            $('#files').fileExif.call(dummy, function (exifObject) {
+                //console.log('fileExif called with ' + files[0].name);
                 var loc = new Location(),
-                    dt = new DateTime(),
-                    latLng;
+                    dt = new DateTime();
+
+                // load location data
                 loc.set ({latLng: {
                     lat: exifObject.GPSLatitude,
                     latRef: exifObject.GPSLatitudeRef,
@@ -19,9 +31,64 @@ $(function() {
                     lngRef: exifObject.GPSLongitudeRef
                 }});
                 loc.coordinateSource = 'camera/phone';
+
+                // load date-time data
                 dt.setFromExifFormat(exifObject.DateTimeOriginal);
 
-                if (loc.decimalLatitude !== null && loc.decimalLongitude !== null) {
+                // display data in image table
+                imageList.injectLocationIntoNewlyAddedImage(files[0].name, loc);
+                imageList.injectDateTimeIntoNewlyAddedImage(files[0].name, dt);
+            });
+        }
+
+/*
+        // called when a file is selected in the file input widget
+        onFileSelect: function (event) {
+            var that = event.data.that,
+                $rows, lastRow,
+                files = this.files,
+                filesIndex = 0;
+
+            console.log('file select called ' + filesIndex);
+
+            imageList.rowAdded();
+
+            if (ExifLoader.geocoder === undefined) { ExifLoader.geocoder = new google.maps.Geocoder(); }
+
+            $(this).multiFileExif(function (exifObject) {
+                console.log('fileExif called ' + filesIndex);
+                console.log('file name is ' + files[filesIndex].name);
+                var loc = new Location(),
+                    dt = new DateTime();
+
+                // load location data
+                loc.set ({latLng: {
+                    lat: exifObject.GPSLatitude,
+                    latRef: exifObject.GPSLatitudeRef,
+                    lng: exifObject.GPSLongitude,
+                    lngRef: exifObject.GPSLongitudeRef
+                }});
+                loc.coordinateSource = 'camera/phone';
+
+                // load date-time data
+                dt.setFromExifFormat(exifObject.DateTimeOriginal);
+
+                // display data in image table
+                imageList.injectLocationIntoNewlyAddedImage(files[filesIndex].name, loc);
+                imageList.injectDateTimeIntoNewlyAddedImage(files[filesIndex].name, dt);
+                filesIndex++;
+                console.log('filesIndex incremented ' + filesIndex);
+
+                // add data to cache in case the table row is not created yet
+                if (files[filesIndex]) {
+                    that.dataCache[files[filesIndex].name] = {
+                        location: loc,
+                        datetime: dt
+                    };
+                }
+
+                */
+/*if (loc.decimalLatitude !== null && loc.decimalLongitude !== null) {
                     latLng = new google.maps.LatLng(loc.decimalLatitude, loc.decimalLongitude);
                     that.geocoder.geocode({'latLng': latLng}, function (results, status) {
                         if (status === google.maps.GeocoderStatus.OK) {
@@ -34,12 +101,15 @@ $(function() {
                     });
                 } else if (dt.isValid()) {
                     ExifPutter.set(dt);
-                }
+                }*//*
+
             });
-        }
-    },
+        }*/
+    };
+
+
     /* ExifPutter handles the disposition of any extracted EXIF data into the user's submission fields. */
-    ExifPutter = {
+    /*var ExifPutter = {
         set: function (dateTime, loc) {
             var screenLoc = new Location().loadFromScreen(),
                 locationConflicts = loc.compare(screenLoc),
@@ -109,10 +179,122 @@ $(function() {
             });
             $("#dateUpdateDialog").dialog('open');
         }
+    },*/
+
+    /* Handles the attributes and behaviour of the list of images to be uploaded. */
+    imageList = {
+        init: function () {
+            var that = this;
+            // wire the 'Use this ..' buttons
+            $('#filesTable').on('click', '.useImageDate', function (e) {
+                var $td = $(this).parents('td'),
+                    dt = $td.data('dateTime');
+                if (dt !== undefined) {
+                    dt.putToScreen();
+                    that.refreshButtonStates('date');
+                }
+            });
+            $('#filesTable').on('click', '.useImageLocation', function (e) {
+                var $td = $(this).parents('td');
+                var loc = $td.data('location');
+                if (loc !== undefined) {
+                    loc.putToScreen();
+                    that.refreshButtonStates('location');
+                }
+            });
+            // listen for external changes to the location so we can update the button states
+            mainMap.addListener({handler: function(mouseEvent, event) { // map drag
+                that.refreshButtonStates('location');
+            }});
+            screenLocation.addListener({handler: function(event, latLng) { // lat/lng input fields
+                that.refreshButtonStates('location');
+            }});
+            // listen for external changes to the date and time so we can update the button states
+            screenDate.addListener({handler: function(event) { // date/time input fields
+                that.refreshButtonStates('date');
+            }});
+        },
+        refreshButtonStates: function (which) {
+            var currentDt = new DateTime().loadFromScreen(),
+                currentLoc = new Location().loadFromScreen(),
+                dt, loc,
+                refreshDate = (which === undefined || which === "date"),
+                refreshLocation = (which === undefined || which === "location");
+            $('#filesTable tr').each(function (i, row) {
+                if (refreshDate) {
+                    dt = $(this).find("td.imageDate").data('dateTime');
+                    //console.log("Date: " + (dt === undefined || currentDt.isSame(dt)));
+                    if (dt === undefined || currentDt.isSame(dt)) {
+                        // then disable this one
+                        $(this).find('button.useImageDate').attr('disabled','disabled');
+                    } else {
+                        // enable 'Use..' button
+                        $(this).find('button.useImageDate').removeAttr('disabled');
+                    }
+                }
+                if (refreshLocation) {
+                    loc = $(this).find("td.imageLocation").data('location');
+                    //console.log("Loc: " + (loc === undefined || currentLoc.isSame(loc)));
+                    if (loc === undefined || currentLoc.isSame(loc)) {
+                        // then disable this one
+                        $(this).find('button.useImageLocation').attr('disabled','disabled');
+                    } else {
+                        // enable 'Use..' button
+                        $(this).find('button.useImageLocation').removeAttr('disabled');
+                    }
+                }
+            });
+        },
+        rowAdded: function () {
+            // minimise 'Add..' button as soon as one image is added
+            $('#addImageButtonLarge').hide();
+            $('#addImageButtonSmall').show();
+        },
+        injectLocationIntoNewlyAddedImage: function (filename, loc) {
+            var $rows = $('#filesTable tr'),
+                name;
+
+            if (loc.isValid()) {
+                // check each row for matching name
+                $.each($rows, function(i, row) {
+                    //console.log("inject location: rows=" + $rows.length +" name=" + filename + " row=" + $(row).find("span.name").html());
+                    if ($(row).find("span.name").html() === filename) {
+                        // inject exif data
+                        // show the value
+                        $(row).find(".imageLatLng").html('Lat: ' + limit(loc.decimalLatitude, 6) +
+                            ', Lng: ' + limit(loc.decimalLongitude, 6));
+                        // store full object as jQuery data
+                        $(row).find("td.imageLocation").data('location', loc);
+                        // make sure the 'Use..' button is enabled
+                        $(row).find("button.useImageLocation").removeAttr('disabled');
+                    }
+                });
+            }
+        },
+        injectDateTimeIntoNewlyAddedImage: function (filename, dt) {
+            var $rows = $('#filesTable tr'),
+                name;
+
+            if (dt.isValid()) {
+                // check each row for matching name
+                $.each($rows, function(i, row) {
+                    if ($(row).find("span.name").html() === filename) {
+                        // inject exif data
+                        // show the value
+                        $(row).find(".imageDateTime").html(dt.getDateForScreen() + ' ' +
+                            dt.getTimeForScreen());
+                        // store full object as jQuery data
+                        $(row).find("td.imageDate").data('dateTime', dt);
+                        // make sure the 'Use..' button is enabled
+                        $(row).find("button.useImageDate").removeAttr('disabled');
+                    }
+                });
+            }
+        }
     };
 
     // wire putter dialogs
-    $("#locationUpdateDialog" ).dialog({
+    /*$("#locationUpdateDialog" ).dialog({
         autoOpen: false,
         draggable: true,
         title: 'Update location from image metadata',
@@ -130,10 +312,12 @@ $(function() {
         show: "blind",
         hide: "explode"
     });
-
+*/
     // wire the file selection widget
-    $('#files').change({that: ExifLoader}, ExifLoader.onFileSelect);
+    // $('#files').change({that: ExifLoader}, ExifLoader.onFileSelect);
 
+    imageList.init();
+    //$(Document).bind('fileuploadsubmit', {that: ExifLoader}, ExifLoader.onAdd);
 });
 
 /*
