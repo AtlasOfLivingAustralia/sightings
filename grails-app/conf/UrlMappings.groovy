3c4339042879ea53c5c98788caead74823a7980d
markew <mark.woolston@axon.com.au>
2012-08-12 07:17:09 +0000

    add action to simulate bookmark service (for devt)
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@48 b53174df-531f-34c8-f11e-a8fbec9bff15

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,10 +1,15 @@
 class UrlMappings {
 
 	static mappings = {
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
+		}
+
+        // pass dummy location bookmarks url
+		"/$controller/$action?/location/user/$userid?"{
 		}
 
 		"/"(controller: 'upload', action:"index")
