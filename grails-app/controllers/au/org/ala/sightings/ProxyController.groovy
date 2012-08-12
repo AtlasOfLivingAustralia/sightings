1533b79b15a706b9cf5703597451a553aa8f02c1
markew <mark.woolston@axon.com.au>
2012-08-12 07:16:04 +0000

    add action to simulate bookmark service (for devt)
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@46 b53174df-531f-34c8-f11e-a8fbec9bff15

--- grails-app/controllers/au/org/ala/sightings/ProxyController.groovy
+++ grails-app/controllers/au/org/ala/sightings/ProxyController.groovy
@@ -43,4 +43,16 @@ class ProxyController {
         render resp as JSON
     }
 
+    /**
+     * for testing only
+     * simulates the bookmark server for testing and development
+     * jsonp
+     */
+    def dummyBookmarks() {
+        def locs = '{"locations":[{"dateCreated":"2012-07-22T02:57:24Z","decimalLatitude":7.181685924530029,"decimalLongitude":79.88414764404297,"locality":"Columbo, Sri Lanka","userId":"mark.woolston@csiro.au","geodeticDatum":"WGS84","coordinateSource":"Google maps","id":"500b6c143004622fc0aa7eb9"},{"dateCreated":"2012-07-19T06:04:28Z","decimalLatitude":-37.575660705566406,"decimalLongitude":143.84042358398438,"locality":"Ballarat","userId":"mark.woolston@csiro.au","geodeticDatum":"WGS84","verbatimLatitude":"-35 13.95 0","verbatimLongitude":"149 1.29 0","coordinateSource":"camera/phone","id":"5007a36c3004622fc0aa7eb8"},{"dateCreated":"2012-07-18T02:13:48Z","decimalLatitude":-35.23249816894531,"decimalLongitude":149.02149963378906,"locality":"6 Ashburner St, Higgins ACT 2615, Australia","userId":"mark.woolston@csiro.au","usingReverseGeocodedLocality":"true","geodeticDatum":"WGS84","verbatimLatitude":"-35 13.95 0","verbatimLongitude":"149 1.29 0","coordinateSource":"camera/phone","id":"50061bdc3004622fc0aa7eb5"},{"dateCreated":"2012-07-18T02:10:51Z","decimalLatitude":-35.23270034790039,"decimalLongitude":149.02220153808594,"locality":"X Ashburner St, Higgins ACT 2615, Australia","userId":"mark.woolston@csiro.au","usingReverseGeocodedLocality":"false","geodeticDatum":"WGS84","verbatimLatitude":"-35 13.96 0","verbatimLongitude":"149 1.33 0","coordinateSource":"camera/phone","id":"50061b2b3004622fc0aa7eb4"},{"dateCreated":"2012-07-18T02:08:25Z","decimalLatitude":-35.23270034790039,"decimalLongitude":149.02220153808594,"locality":"1 Ashburner St, Higgins ACT 2615, Australia","userId":"mark.woolston@csiro.au","usingReverseGeocodedLocality":"true","geodeticDatum":"WGS84","verbatimLatitude":"-35 13.96 0","verbatimLongitude":"149 1.33 0","coordinateSource":"camera/phone","id":"50061a993004622fc0aa7eb3"},{"dateCreated":"2012-07-17T01:17:18Z","decimalLatitude":-29.89080047607422,"decimalLongitude":130.84190368652344,"locality":"Desert","userId":"mark.woolston@csiro.au","geodeticDatum":null,"verbatimLatitude":null,"coordinateSource":"physical map","verbatimLongitude":null,"otherSource":null,"physicalMapScale":null,"id":"5004bd1e3004622fc0aa7ea5"},{"dateCreated":"2012-07-16T05:26:15Z","decimalLatitude":-35.22999954223633,"decimalLongitude":149.02000427246094,"locality":"18 Rich Pl, Higgins ACT 2615, Australia","userId":"mark.woolston@csiro.au","geodeticDatum":null,"verbatimLatitude":null,"coordinateSource":"camera/phone","verbatimLongitude":null,"otherSource":null,"physicalMapScale":null,"id":"5003a5f73004622fc0aa7ea4"},{"dateCreated":"2012-07-16T05:09:59Z","decimalLatitude":-35.23231887817383,"decimalLongitude":149.02178955078125,"locality":"Higgins ACT 2615, Australia","userId":"mark.woolston@csiro.au","geodeticDatum":null,"verbatimLatitude":null,"coordinateSource":"camera/phone","verbatimLongitude":null,"otherSource":null,"physicalMapScale":null,"id":"5003a2273004622fc0aa7ea3"}]}'
+        if (params.callback) {
+            locs = params.callback + "(" + locs + ")"
+        }
+        render (contentType: "application/json", text: locs)
+    }
 }
