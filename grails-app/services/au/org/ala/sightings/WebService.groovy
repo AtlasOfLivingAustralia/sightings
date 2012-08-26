f868b5ecbd7c69263cea6cb9064d499d32e6b03d
markew <mark.woolston@axon.com.au>
2012-08-26 06:00:24 +0000

    add support for GET
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@98 b53174df-531f-34c8-f11e-a8fbec9bff15

--- grails-app/services/au/org/ala/sightings/WebService.groovy
+++ grails-app/services/au/org/ala/sightings/WebService.groovy
@@ -1,8 +1,48 @@
 package au.org.ala.sightings
 
 import grails.converters.JSON
+import org.codehaus.groovy.grails.web.converters.exceptions.ConverterException
 
 class WebService {
+
+    def get(String url) {
+        def conn = new URL(url).openConnection()
+        try {
+            conn.setConnectTimeout(10000)
+            conn.setReadTimeout(50000)
+            return conn.content.text
+        } catch (SocketTimeoutException e) {
+            def error = [error: "Timed out calling web service. URL= \${url}."]
+            println error.error
+            return error as JSON
+        } catch (Exception e) {
+            def error = [error: "Failed calling web service. ${e.getClass()} ${e.getMessage()} URL= ${url}."]
+            println error.error
+            return error as JSON
+        }
+    }
+
+    def getJson(String url) {
+        def conn = new URL(url).openConnection()
+        try {
+            conn.setConnectTimeout(10000)
+            conn.setReadTimeout(50000)
+            def json = conn.content.text
+            return JSON.parse(json)
+        } catch (ConverterException e) {
+            def error = ['error': "Failed to parse json. ${e.getClass()} ${e.getMessage()} URL= ${url}."]
+            println error.error
+            return error
+        } catch (SocketTimeoutException e) {
+            def error = [error: "Timed out getting json. URL= \${url}."]
+            println error.error
+            return error
+        } catch (Exception e) {
+            def error = [error: "Failed to get json from web service. ${e.getClass()} ${e.getMessage()} URL= ${url}."]
+            println error.error
+            return error
+        }
+    }
 
     def doPost(String url, String postBody) {
         //println "WebService:" + postBody
