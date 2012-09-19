c1ee22e09f5330c2aa0aac387434377310c60b2f
markew <mark.woolston@axon.com.au>
2012-09-19 03:01:11 +0000

    add sort options for listing user records add ability to list records from the biocache
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@129 b53174df-531f-34c8-f11e-a8fbec9bff15

--- grails-app/controllers/au/org/ala/sightings/RecordsController.groovy
+++ grails-app/controllers/au/org/ala/sightings/RecordsController.groovy
@@ -6,21 +6,53 @@ class RecordsController {
     def username = 'mark.woolston@csiro.au' // until CAS is integrated
 
     def user() {
+        // handle sort options
+        def opts = ""
+        if (params.sort) {
+            opts += "?sort=" + params.sort
+        }
+        if (params.order) {
+            opts += (opts ? "&" : "?") + "order=" + params.order
+        }
+        //println opts
         // get records for current user
         def records
         if (grailsApplication.config.mock.records.service) {
             records = ProxyController.getRecords()
         } else {
             records = webService.getJson(grailsApplication.config.ala.recordsServerURL +
-            "user/" + username)
+            "user/" + username + opts)
             if (records.error) {
                 // TODO: handle service errors
                 println records.error
             }
             records = records.records
         }
-        println records
+        //println records
         [records: records]
+    }
+
+    /**
+     * Get real records from the biocache so we can view them in this context.
+     * This is just a sanity check used during development.
+     * @param username the email/username of the collector
+     */
+    def realuser() {
+        def resp = webService.getJson(grailsApplication.config.biocache.baseURL +
+                "ws/occurrences/search.json?q=data_resource_uid:dr364&fq=user_id:${params.user}")
+        println resp
+        def records = resp.occurrences.collect {
+            [scientificName: it.scientificName,
+             commonName: it.vernacularName,
+             eventDate: "2012-09-01T00:00:00Z",// it.eventDate,
+             guid: it.occurrenceID,
+             decimalLatitude: it.decimalLatitude,
+             decimalLongitude: it.decimalLongitude,
+             coordinateUncertaintyInMeters: it.coordinateUncertaintyInMeters
+             ]
+        }
+
+        render( view: 'user', model: [records: records] )
     }
 
     def delete() {
