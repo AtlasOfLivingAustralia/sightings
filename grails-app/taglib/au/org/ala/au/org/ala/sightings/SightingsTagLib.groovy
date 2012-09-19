93136c282b4cb4b942c5e409cbf2217abf208d51
markew <mark.woolston@axon.com.au>
2012-09-19 03:02:14 +0000

    add tag for formatting dates
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@131 b53174df-531f-34c8-f11e-a8fbec9bff15

---
+++ grails-app/taglib/au/org/ala/au/org/ala/sightings/SightingsTagLib.groovy
@@ -0,0 +1,29 @@
+package au.org.ala.au.org.ala.sightings
+
+import java.text.SimpleDateFormat
+
+class SightingsTagLib {
+
+    static namespace = "si"
+    static outFormat = new SimpleDateFormat("d MMM yyyy HH:mm")
+    //static outFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm")
+
+    def formatDate = { attrs ->
+        println "date: " + attrs.date
+        log.debug 'formatting date: ' + attrs.date
+        def dateStr
+        if (attrs.date) {
+            def date = new Date().parse("yyyy-MM-dd'T'HH:mm:ss'Z'", attrs.date)
+            dateStr = outFormat.format(date)
+            // remove time if it is 00:00
+            if (dateStr.size() > 15 && dateStr[-5..-1] == "00:00") {
+                dateStr -= dateStr[-5..-1]
+            }
+        }
+        else {
+            dateStr = ''
+        }
+        out << dateStr
+    }
+
+}
