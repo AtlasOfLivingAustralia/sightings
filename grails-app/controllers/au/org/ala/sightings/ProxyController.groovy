069a99ff8f1342a95e4ec017ff40d220efa31ace
djtfmartin <djtfmartin@gmail.com>
2012-12-07 08:13:23 +0000

    brackets
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/sightings/trunk@284 b53174df-531f-34c8-f11e-a8fbec9bff15

--- grails-app/controllers/au/org/ala/sightings/ProxyController.groovy
+++ grails-app/controllers/au/org/ala/sightings/ProxyController.groovy
@@ -144,7 +144,7 @@ class ProxyController {
         if (target) {
             mockRecords - target
         }
-        log.debug("record ${id} deleted")
+        log.debug("record deleted" + id)
     }
 
     def reloadConfig = {
